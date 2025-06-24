6a0c8c39343942d54cbd1981cf9dde37f80f61f3
Hacademy <zdlsz@hanmail.net>
2025-06-24 11:18:11 +0900

    Create DataExample03.java

---
+++ EP-008/DataExample03.java
@@ -0,0 +1,16 @@
+public class DataExample03 {
+    public static void main(String[] args) {
+        //실수
+        float a = 1.23f;
+        //double b = 1.23d;
+        double b = 1.23;
+        
+        System.out.println(a);
+        System.out.println(b);
+        
+        float c = 1.23423482348238942398429384f;
+        double d = 1.23498239482938429384239842;
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
